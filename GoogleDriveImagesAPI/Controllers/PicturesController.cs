7d810b6909bbe119c209b053c3c280d1e0f160c3
Jon Coello <joncoello@gmail.com>
2017-07-10 20:14:44 +0100

    Add project files.

---
+++ GoogleDriveImagesAPI/Controllers/PicturesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GoogleDriveImagesAPI.Controllers
+{
+    [RoutePrefix("api/pictures")]
+    public class PicturesController : ApiController
+    {
+
+        [Route("")]
+        public string Get() {
+            return "hello world";
+        }
+
+    }
+}
